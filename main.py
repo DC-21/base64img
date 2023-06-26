8ea49697382e6e598a30e804f9ed07dd654828df
DC-21 <cholahkuboko@gmail.com>
2023-06-26 10:39:47 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import  base64
+
+file = open('example.txt', 'rb')
+encoded_data = file.read()
+file.close()
+
+decoded_data = base64.b64decode((encoded_data))
+
+img_file = open('img.png', 'wb')
+img_file.write(decoded_data)
+img_file.close()
